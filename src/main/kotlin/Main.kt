e6f219d955849ab83b6714c5660570717b8782ef
Loice-KaniniMwau <mwauloice@gmail.com>
2023-02-04 21:48:32 +0300

    my kotlin practise

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+
+    var names= arrayOf("mary@gmail","faith@gmail","maryanne@gmail")
+    var emails= arrayOf(".com",".com", ".com")
+    var stringresult= arrayOf("names[0]emails[0]", "names[1]emails[1]", "names[2] emails[2]")
+    println(stringresult)
+
+
+}
