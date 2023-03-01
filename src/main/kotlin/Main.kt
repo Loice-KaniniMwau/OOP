b306bba26f36782622f75f0bbfc8b55bf00b5f1d
Loice-KaniniMwau <mwauloice@gmail.com>
2023-03-01 07:32:06 +0300

    OOP-ASSIGNMENT

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,31 @@
-fun main(args: Array<String>) {
 
-    var names= arrayOf("mary@gmail","faith@gmail","maryanne@gmail")
-    var emails= arrayOf(".com",".com", ".com")
-    var stringresult= arrayOf("names[0]emails[0]", "names[1]emails[1]", "names[2] emails[2]")
-    println(stringresult)
 
+fun main() {
+    val identity=Human("loice",23,56)
+    identity.eat(5)
+    identity.speak("that's her")
+    identity.birthday()
+ var theUser=User("mary","kamau","mwauloice@gmail.com","+254707562960","mary2023")
+println(theUser.phoneNumber)
+    println(theUser.email)
+
+}
+class Human(var name:String,var age:Int,var weight:Int){
+    fun eat(foodweight:Int) {
+   println("I am eating $foodweight kgs of food")
+        weight +=foodweight
+        println(weight)
+
+    }
+    fun speak(speech:String){
+        println(speech)
+
+    }
+    fun birthday(){
+        ++age
+        println(age)
+    }
+}
+data class User(var firstName:String,var lastName:String,val email:String,var phoneNumber:String,var password:String){
 
 }
